31fb1ff14241558dfa66fdad1d57deebcbd9d3b6
FirmanMulyawan <firmanmulyawan491@gmail.com>
2019-10-24 15:27:33 +0700

    membetulkan nomor 5

--- nomor 5/solusiBilbul.js
+++ nomor 5/solusiBilbul.js
@@ -3,16 +3,16 @@ var x, y, z,
     result = document.getElementById("result");
 
 btn.addEventListener("click", function () {
-    // var hasil;
+    var hasil = "";
     for (x = 0; x <= 25; x++) {
         for (y = 0; y <= 25; y++) {
             for (z = 0; z <= 25; z++) {
                 if (x + y + z === 25) {
-                    console.log(x + " + " + y + " + " + z + " = 25 ");
+                    hasil = hasil + (x + " + " + y + " + " + z + " = 25 <br><br>");
                 }
             }
         }
 
     }
-    // result.innerHTML = hasil;
+    result.innerHTML = hasil;
 });
